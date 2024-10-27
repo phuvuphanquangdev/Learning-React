7574ff3eae44da4cf2ffc195761c1540ad7e1945
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-27 15:12:38 +0700

    Naming Convention

---
+++ 04-create-react-app/cra-conventions/src/Dog.js
@@ -0,0 +1,32 @@
+// import React, { Component } from 'react';
+// import pug from './pug.jpeg';
+// import './Dog.css';
+
+// class Dog extends Component {
+// 	render() {
+// 		return (
+// 			<div className="Dog">
+// 				<h1>DOG!</h1>
+// 				<img className="Dog-img" src={pug} />
+// 			</div>
+// 		);
+// 	}
+// }
+// export default Dog;
+
+import React, {Component} from 'react';
+import pug from './pug.jpeg';
+import './Dog.css';
+
+class Dog extends Component {
+	render() {
+		return (
+			<div className="Dog">
+				<h1>Dog!</h1>
+				<img className="Dog-img" src={pug}/>
+			</div>
+		);
+	}
+}
+
+export default Dog;
